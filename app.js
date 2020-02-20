4dda5ece103269f83284ed130efa4258430d44d8
Huynh <huynhp24@gmail.com>
2020-02-20 09:48:34 -0800

    Navbar coolness

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const express = require('express')
-const app = express()
 
-app.use(express.static('public'))
+var express =require('express'),
+    routes = require('./routes/routes.js'),
+    http = require('http'),
+    path = require('path'),
+    app = express();
 
-app.post('/', homePage)
-
-function homePage(req, res){
-  console.log('You are on the home page')
-}
-
-app.listen(8081, function() {
-  console.log('Source Scan app listening on port 8081')
+app.set('port', 8081);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/style', express.static(path.join(__dirname, '/public/style')));
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
+
+//Routes
+app.get('/', routes.home);
+
+// Start Server
+http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/routes.js
@@ -0,0 +1,6 @@
+//var soc2 = require('../soc2Backend.js');
+
+exports.home = function(req, res) {
+    console.log("AHHHHHHHHHHHHHHHH");
+    res.render('navbar.html');
+};
