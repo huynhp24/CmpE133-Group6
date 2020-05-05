ee47c67901d129278b1760ca7ca97addd8254120
Galic-parmesan-wings <53062441+Galic-parmesan-wings@users.noreply.github.com>
2020-05-05 02:14:57 -0700

    chris3

--- app.js
+++ app.js
@@ -14,11 +14,17 @@ const express =require('express'),
     checkAuthenticated = require('./component/login_component/checkAuthenticated'); //if user is not authenticated, not allow to access pages other than login, register, index pages(CHRIS)
     checkNotAuthenticated = require('./component/login_component/checkNotAuthenticated');//if user is authenticated, redirect to home page if user go to /login page(CHRIS)
 
-app.use(
-	bodyParser.urlencoded({
-    	extended: true,
-	})
-);
+
+app.set('port', 8081);
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/style', express.static(path.join(__dirname, '/public/style')));
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 app.use(session({
 	secret: 'secret',
@@ -28,12 +34,20 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-
+app.use(passport.initialize()); // for login(CHRIS)
+app.use(passport.session()); //for login(CHRIS)
+app.use(methodOverride('_method')); //for logout(CHRIS)
 //End Mysql connector
 
 //Routes
-app.get('/', routes.home);
-app.get('/testQuery', routes.testQuery);
+app.get('/', routes.index);//Introduction page (CHRIS)
+app.get('/home',checkAuthenticated, routes.home);// home page that authenticated user will see right after login(CHRIS)
+app.get('/login',checkNotAuthenticated, routes.renderLoginPage); // render login page(CHRIS)
+app.get('/register',checkNotAuthenticated, routes.renderSignupPage); // render register page(CHRIS)
+app.post('/register',checkNotAuthenticated, routes.handleRegisterForm); // handle registration form data(CHRIS)
+app.post('/login',checkNotAuthenticated, routes.handleLoginForm);// handle login form data(CHRIS)
+app.delete('/logout',routes.logout);// logout(CHRIS)
+//app.get('/testQuery', routes.testQuery);
 app.get('/test', routes.test);
 app.get('/getBackValue', routes.backValue);
 app.get('/viewSchedule', routes.viewSchedule);
